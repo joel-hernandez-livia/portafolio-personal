23836c0c2e72a03937ddb089338b624c9115e58a
HernandezLivia <joel.sw26@gmail.com>
2023-08-25 11:48:41 -0500

    correccion de la asignacion de eventos en el menu del header en la version movil

--- scripts.js
+++ scripts.js
@@ -2,3 +2,11 @@ const bars = document.querySelector('.bars');
 const navbar = document.querySelector('.navbar');
 
 bars.addEventListener('click',()=>{navbar.classList.toggle('active')})
+
+const colapsarItems = document.querySelectorAll('.colapsar');
+colapsarItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navbar.classList.remove('active');
+    console.log("Menú colapsado");
+  });
+});
